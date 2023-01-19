cdb36a39e3a985e68733d0b2e058d4d4d2dabdcd
Yasin Can <canyasin1996@gmail.com>
2023-01-20 00:30:28 +0300

    responsive design improvements

--- js/initializeSwiper.js
+++ js/initializeSwiper.js
@@ -20,4 +20,26 @@ const swiper = new Swiper('.swiper', {
 
     clickable: true,
   },
+  breakpoints: {
+    320: {
+      slidesPerView: 1,
+      spaceBetween: 10,
+    },
+    480: {
+      slidesPerView: 1,
+      spaceBetween: 10,
+    },
+    640: {
+      slidesPerView: 2,
+      spaceBetween: 20,
+    },
+    768: {
+      slidesPerView: 2,
+      spaceBetween: 20,
+    },
+    1024: {
+      slidesPerView: 2,
+      spaceBetween: 20,
+    },
+  },
 });
